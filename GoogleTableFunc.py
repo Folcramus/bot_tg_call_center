c6d2c865d87a03fd51ba30f3c0746d82c1cad063
Folcramus <ponopaytov@Gmail.com>
2023-10-17 12:32:11 +0300

    Тест деплоя v1

--- GoogleTableFunc.py
+++ GoogleTableFunc.py
@@ -2,19 +2,20 @@ import gspread
 
 
 def ConnTable():
-    gc = gspread.service_account(filename=r'C:\Users\ponop\Downloads\file.json')
+    gc = gspread.service_account(filename=r'file.json')
 
-    sh = gc.open_by_key('1bd1QoaY-O60n3AYY_MjTFQtIOTG5LBMp8o8IcU7e2ac')
+    sh = gc.open_by_key('1qSJncekRpbXCmkLCuLnNZC6ugPVnnpNoYhtSlniZVoM')
 
     return sh
 
 
 def GetPhoneTable(phone):
     sh = ConnTable()
-    data = sh.sheet1.get_all_records()
-
+    data = sh.sheet1.acell('B1').value
+    print(data)
     for i in range(0,len(data)):
         if data[i]["Телефон"] == phone:
             return data[i]
 
 
+GetPhoneTable(1)
